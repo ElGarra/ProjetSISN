903254e5d7522da0c35daac8c7c01ccf63f33028
ElGarra <joaquincastanos@uc.cl>
2023-05-16 16:16:51 +0200

    Creating main menu and project structure

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.menu import Menu
+
+
+# Run the main code
+if __name__ == "__main__":
+    menu = Menu()
+    menu.execute()
---
+++ src/menu.py
@@ -0,0 +1,47 @@
+"""
+Script created to work as interactive menu during the execution of the code.
+It works in the terminal, please follow the instructions.
+"""
+import sys
+
+class Menu():
+
+    def __init__(self) -> None:
+        print("####################################################################################################\n")
+        print(" --> Welcome to the interactive console of the project 'Methods for improving image quality'\n\n")
+        self.main_question = " --> What would you like to do now? Please choose an option:\n\n \
+        [1] Apply all filters in all images\n \
+        [2] Apply all filters in one image\n \
+        [3] Apply one filter in all images\n \
+        [4] Apply one filter in one image\n \
+        [5] Exit\n\n"
+        self.error_message = "\nInvalid command, please try again.\n"
+        self.main_options_list = list(range(1, 6))
+
+    def options(self):
+        print("####################################################################################################\n")
+        option = int(input(self.main_question))
+        if not option in self.main_options_list:
+            print(self.error_message)
+            option = int(input(self.main_question))
+        else:
+            if option == 1:
+                # Call respective function for 1
+                print(option)
+            elif option == 2:
+                # Call respective function for 2
+                print(option)
+            elif option == 3:
+                # Call respective function for 3
+                print(option)
+            elif option == 4:
+                # Call respective function for 4
+                print(option)
+            else:
+                sys.exit(0)
+
+    def execute(self):
+        self.options()            
+
+
+        
