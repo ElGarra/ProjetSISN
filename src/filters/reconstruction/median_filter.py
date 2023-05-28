82c74cb1e73d6766772da7c62417756fc8e3ee8d
ElGarra <joaquincastanos@uc.cl>
2023-05-28 19:07:10 +0200

    deleting comments

--- src/filters/reconstruction/median_filter.py
+++ src/filters/reconstruction/median_filter.py
@@ -19,17 +19,6 @@ class MedianFilter():
     def calculate_difference(self):
         self.difference = self.image - self.rebuilt_image
 
-    # def generate_histogram(self):
-    #     self.hist, self.bins = np.histogram(self.difference.flatten(), bins=256, range=(-1, 1))
-    #     # Plot and save the histogram as a PNG image
-    #     plt.figure(figsize=(8, 4))
-    #     plt.plot(self.bins[:-1], self.hist, color='black')
-    #     plt.title('Histogram of the difference (Reconstruction)')
-    #     plt.xlabel('Pixel value')
-    #     plt.ylabel('Frequency')
-    #     plt.savefig(f'assets/aditiveGaussianDegradationMedianReconstruction/{self.image_name}/histogram_reconstruction_{self.image_name}.png', dpi=300)
-    #     plt.close()
-
     def save_images(self):
        # Save the reconstrucred image as PNG
         io.imsave(f'assets/aditiveGaussianDegradationMedianReconstruction/{self.image_name}/median_rebuilt_{self.image_name}.png', util.img_as_ubyte(self.rebuilt_image))
@@ -48,11 +37,6 @@ class MedianFilter():
         axes[2].imshow(self.difference, cmap='gray')
         axes[2].set_title('Difference')
         axes[2].axis('off')
-        # axes[3].plot(self.bins[:-1], self.hist, color='black')
-        # axes[3].set_title('Histogram of Difference')
-        # axes[3].set_xlabel('Pixel Value')
-        # axes[3].set_ylabel('Frequency')
-        
         plt.suptitle('Median Filter Analysis')
         plt.tight_layout()
         plt.show()
